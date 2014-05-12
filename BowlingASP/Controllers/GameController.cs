24120fcf4026edc88ba34ef76e29cc4a23fb4518
Benjamin Sénéchal <senechal.benjamin1@gmail.com>
2014-05-12 12:12:07 +0200

    Add reference

---
+++ BowlingASP/Controllers/GameController.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace BowlingASP.Controllers
+{
+    public class GameController : Controller
+    {
+        //
+        // GET: /Game/
+
+        public ActionResult Index()
+        {
+            GameService.ServiceGameClient client = new GameService.ServiceGameClient();
+            List<GameService.game> list = new List<GameService.game>();
+            list = client.findAll("0");
+            return View(list);
+        }
+
+        public ActionResult Create()
+        {
+            GameService.ServiceGameClient client = new GameService.ServiceGameClient();
+            PlayerService.ServicePlayerClient clientPlayer = new PlayerService.ServicePlayerClient();
+            return View();
+        }
+    }
+}
